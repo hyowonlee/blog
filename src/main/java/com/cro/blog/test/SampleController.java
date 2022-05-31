8a71ee96b67ae9042acb48c301bee1b95a526960
hyowon <gydnjs2007@naver.com>
2022-05-31 22:34:39 +0900

    2022-05-31 - application.yml 세팅, jsp 연결 환경설정 17강

---
+++ src/main/java/com/cro/blog/test/SampleController.java
@@ -0,0 +1,15 @@
+package com.cro.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/test")
+    public String Hello()
+    {
+        System.out.println("test/SampleController");
+        return "test";
+    }
+}
