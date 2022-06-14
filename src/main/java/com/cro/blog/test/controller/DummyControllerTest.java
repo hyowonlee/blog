cf052b26381ba9ae9d547867ee154338ea410ef0
hyowon <gydnjs2007@naver.com>
2022-06-14 19:16:05 +0900

    2022-06-14 - 페이징 테스트 27강

--- src/main/java/com/cro/blog/test/controller/DummyControllerTest.java
+++ src/main/java/com/cro/blog/test/controller/DummyControllerTest.java
@@ -4,9 +4,14 @@ import com.cro.blog.model.RoleType;
 import com.cro.blog.model.User;
 import com.cro.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
@@ -45,7 +50,7 @@ public class DummyControllerTest {
     }
     //객체가 아닌 변수로 받기
 //    @PostMapping("/dummy/join")
-//    public void testJoin(String username, String password, String email) // view에서 태그의 속성인 name과 변수명이 같다면 @RequestParame("username") 필요없음
+//    public void testJoin(@RequestParame("username") String username, String password, String email) // view에서 태그의 속성인 name과 변수명이 같다면 @RequestParame("username") 필요없음
 //    {
 //        System.out.println(id);
 //        System.out.println(password);
@@ -91,4 +96,28 @@ public class DummyControllerTest {
         return user;
     }
 
+    // 전체 유저 조회
+    @GetMapping("/dummy/search/list")
+    @ResponseBody
+    public List<User> listTestSearchGet()
+    {
+        System.out.println("DummyControllerTest.listTestSearchGet");
+        return userRepository.findAll();
+    }
+
+    // jpa는 페이징 할 수 있게 데이터를 select해주는 라이브러리 제공, jsp에서 하려면 우리가 로직을 다 짜야됨
+    // 한페이지당 2건의 데이터를 리턴받아볼 예정
+    @GetMapping("/dummy/search/list/page")
+    @ResponseBody
+    public List<User> listTestSearchGetPageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable)
+    {
+        System.out.println("DummyControllerTest.listTestSearchGetPageList");
+        //Page<User> pageList = userRepository.findAll(pageable); // Pageable 객체를 입력받는 함수로 간단히 페이징 가능
+
+        // 위에껄로 받으면 데이터가 pageable데이터도 같이와서 지금은 필요없으니 content데이터만 가져오려고 .getContent()로 content데이터만 가져옴
+        // Page 객체는 마지막페이지인지 첫 페이지인지 .isLast() .isFirst() 이런 함수들을 제공함
+        Page<User> pageList = userRepository.findAll(pageable);
+        List<User> userPageList = pageList.getContent(); // 리스트로 받아서 데이터만 리턴해주는게 좋다
+        return userPageList;
+    }
 }
--- src/main/webapp/WEB-INF/views/test.jsp
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -9,6 +9,7 @@
 <body>
 <h1>this page is test page</h1>
 <form action="/dummy/join" method="post">
+    <label>join</label>
     <input type="text" name="username" placeholder="username">
     <input type="password" name="password" placeholder="password">
     <input type="text" name="email" placeholder="email">
@@ -16,8 +17,14 @@
     <button>submit</button>
 </form>
 <form action="/dummy/search" method="get">
+    <label>search</label>
     <input type="text" name="id">
     <button>search</button>
+</form>
+<form action="/dummy/search/list/page">
+    <input type="text" name="page">
+    <label>pageSearch (start = 0, size = 2)</label>
+    <button>submit</button>
 </form>
 </body>
 </html>
